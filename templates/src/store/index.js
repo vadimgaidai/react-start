f5bea6a4fa1862a3d38f9825c4720c5718592992
Vadim Gaidai <vadim.gaidai@icloud.com>
2020-01-23 20:20:44 +0200

    add create-react-app

--- templates/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import { connectRouter } from 'connected-react-router'
-import { TestReducer } from './test/reducer'
-
-export default (history) => combineReducers({
-  router: connectRouter(history),
-  test: TestReducer
-})
